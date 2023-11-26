80ac7726038b2cee2addf5714c2074c95c054559
Rafael Garcia Ruiz <llerrak@hotmail.com>
2023-11-26 17:50:40 +0100

    Start project for android studio
    Main folder of the project added
    
    Signed-off-by: Rafael Garcia Ruiz <llerrak@hotmail.com>

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.myapplication;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Setup Server information
+    protected static String server = "192.168.1.133";
+    protected static int port = 7070;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Capturamos el boton de Enviar
+        View button = findViewById(R.id.button_send);
+
+        // Llama al listener del boton Enviar
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+
+
+    }
+
+    // Creación de un cuadro de dialogo para confirmar pedido
+    private void showDialog() throws Resources.NotFoundException {
+        CheckBox sabanas = (CheckBox) findViewById(R.id.checkBox_sabanas);
+
+        if (!sabanas.isChecked()) {
+            // Mostramos un mensaje emergente;
+            Toast.makeText(getApplicationContext(), "Selecciona al menos un elemento", Toast.LENGTH_SHORT).show();
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("Enviar")
+                    .setMessage("Se va a proceder al envio")
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                                // Catch ok button and send information
+                                public void onClick(DialogInterface dialog, int whichButton) {
+
+                                    // 1. Extraer los datos de la vista
+
+                                    // 2. Firmar los datos
+
+                                    // 3. Enviar los datos
+
+                                    Toast.makeText(MainActivity.this, "Petición enviada correctamente", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                    )
+                    .
+
+                            setNegativeButton(android.R.string.no, null)
+
+                    .
+
+                            show();
+        }
+    }
+
+
+}
